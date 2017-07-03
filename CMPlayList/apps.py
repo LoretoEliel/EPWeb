8527826171b086ef59896febe0cc32e24f0800be
EliDjangoDev <EliDjangoDev@gmail.com>
2017-07-03 15:50:19 -0400

    validacion de campos existentes

--- CMPlayList/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class CmplaylistConfig(AppConfig):
-    name = 'CMPlayList'
--- contenido/models.py
+++ contenido/models.py
@@ -6,10 +6,10 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class libro(models.Model):
-	titulo = models.CharField(max_length=150, blank=True, null=True)
+	titulo = models.CharField(max_length=150, blank=True, null=True, unique=True)
 	escritor = models.CharField(max_length=150, blank=True, null=True)
 	descripcion = models.TextField(max_length=5000, blank=True, null=True)
-	libro = models.URLField(max_length=2000, null=True, blank=True, default='')
+	libro = models.URLField(max_length=2000, null=True, blank=True, default='', unique=True)
 	categoria = models.CharField(max_length=150, null=True, blank=True)
 	subida = models.DateTimeField(auto_now_add=True, auto_now=False)
 	actualizado = models.DateTimeField(auto_now_add=False, auto_now=True)
